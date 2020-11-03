8f7c57e33993393384220bca55f9d2acd79d2963
Kleber Freire <ch4r4d4@gmail.com>
2020-11-02 21:47:17 -0300

    Menu lateral Pronto

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,48 @@
 import React from 'react'
-import { Container } from './style'
+import { 
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp 
+} from 'react-icons/md'; 
+
+import logoImg from '../../assets/logo.svg'
+
+import { 
+  Container,
+  Header,
+  LogoImg,
+  MenuContainer,
+  MenuItemLink,
+  Title 
+} from './style'
 
 const Aside: React.FC = () => {
   return (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <LogoImg src={logoImg} alt="Logo Minha Carteira" />
+        <Title>Minha Carteira</Title>
+      </Header>
+      <MenuContainer>
+        <MenuItemLink href="#">
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <MdArrowUpward />
+          Entradas
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <MdArrowDownward />
+          Saídas
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <MdExitToApp />
+          Sair
+        </MenuItemLink>
+      </MenuContainer>
+
     </Container>
   )
 }
